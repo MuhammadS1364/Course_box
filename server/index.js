e8902346e16bbc5a5eeac12c497eebd7b587bb1c
_Muhammad_Shafee_ <shafeealam1364@outlook.com>
2026-01-09 09:05:31 +0530

    Moduels Configruation installed

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config()
