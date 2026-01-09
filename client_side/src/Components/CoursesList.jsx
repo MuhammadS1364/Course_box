93f327e5cc479562907e8d45d3528e6eafbc26ff
_Muhammad_Shafee_ <shafeealam1364@outlook.com>
2026-01-09 15:30:23 +0530

    Shifting to Other platform

---
+++ client_side/src/Components/CoursesList.jsx
@@ -0,0 +1,8 @@
+
+
+
+export default function CourseList() {
+    return <div>
+        <h1>Add CourseList page.........</h1>
+    </div>
+}
---
+++ client_side/src/Components/Forms.jsx
@@ -0,0 +1,7 @@
+
+
+export default function formsApp() {
+    return <div>
+        <h1>Add forms page.........</h1>
+    </div>
+}
